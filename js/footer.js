83b566f7847040267c4bd0f13970f798184b541e
Rodrigo <ezequielcomar86@gmail.com>
2023-07-02 16:40:11 -0300

    Resolucion de bugs en modal favoritos

--- js/footer.js
+++ js/footer.js
@@ -4,14 +4,17 @@ let home = document.getElementById("home");
 let fav = document.getElementById("favorite");
 let modal = document.getElementById("modal");
 let cerrar = document.getElementById("close");
+let body = document.getElementById("body");
 home.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
 fav.addEventListener("click", () => {
   modal.classList.toggle("show");
+  body.classList.toggle("over");
 });
 
 cerrar.addEventListener("click", () => {
   modal.classList.remove("show");
+  body.classList.remove("over");
 });
