87285190029530eab1987a51203ad8252c29fd6d
Rodrigo <ezequielcomar86@gmail.com>
2023-07-04 18:59:51 -0300

    Finalizacion de modal cart

--- js/footer.js
+++ js/footer.js
@@ -1,19 +1,26 @@
-"use strict";
-
+// "use strict";
+import { modalFavorite, modalCart } from "./modales.js";
 let home = document.getElementById("home");
 let fav = document.getElementById("favorite");
 let modal = document.getElementById("modal");
 let cerrar = document.getElementById("close");
 let body = document.getElementById("body");
+let cart = document.getElementById("cart");
+let conteinerCards = document.getElementById("conteinerCardsFav");
 home.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
 fav.addEventListener("click", () => {
-  modal.classList.toggle("show");
-  body.classList.toggle("over");
+  modal.classList.add("show");
+  body.classList.add("over");
+  conteinerCards.innerHTML = modalFavorite;
 });
-
+cart.addEventListener("click", () => {
+  modal.classList.add("show");
+  body.classList.add("over");
+  conteinerCards.innerHTML = modalCart;
+});
 cerrar.addEventListener("click", () => {
   modal.classList.remove("show");
   body.classList.remove("over");
---
+++ js/modales.js
@@ -0,0 +1,159 @@
+"use strict";
+
+export const modalFavorite = `
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/chaqueta.webp" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through">$7680</p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p>4% OFF</p>
+    </span>
+    <span class="send">
+      <p>Envio gratis</p>
+      <i class="bx bxs-plane-take-off"></i>
+      <p>FULL</p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/ropaInterior.png" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through"></p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p></p>
+    </span>
+    <span class="send">
+      <p></p>
+      <!-- <i class="bx bxs-plane-take-off"></i> -->
+      <p></p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/pantalon.jpg" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through">$7680</p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p>4% OFF</p>
+    </span>
+    <span class="send">
+      <p>Envio gratis</p>
+      <i class="bx bxs-plane-take-off"></i>
+      <p>FULL</p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/remera.webp" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through"></p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p></p>
+    </span>
+    <span class="send">
+      <p></p>
+      <!-- <i class="bx bxs-plane-take-off"></i> -->
+      <p></p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/zapatos.webp" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through">$7680</p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p>4% OFF</p>
+    </span>
+    <span class="send">
+      <p>Envio gratis</p>
+      <i class="bx bxs-plane-take-off"></i>
+      <p>FULL</p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>
+<article class="article_card_fav">
+  <section class="article_img">
+    <img src="img/ropaInterior.png" alt="" />
+  </section>
+  <section class="article_text">
+    <p class="title_fav">
+      Campera Mujer de algodon con piel en el interior
+    </p>
+    <p class="through"></p>
+    <span class="ofert">
+      <p>$8000</p>
+      <p></p>
+    </span>
+    <span class="send">
+      <p></p>
+      <!-- <i class="bx bxs-plane-take-off"></i> -->
+      <p></p>
+    </span>
+    <p class="delete">Eliminar</p>
+  </section>
+</article>`;
+
+export const modalCart = `
+<article class="card-cart">
+<section class="cart-top">
+  <p>Comprador</p>
+</section>
+<section class="cart-mid">
+  <div class="conteiner-img-cart">
+    <img src="img/chaqueta.webp" alt="" />
+  </div>
+  <div class="conteiner-text-cart">
+    <p>Campera frizada buzo con capucha</p>
+    <p>color: gris, talle: M</p>
+    <p>Eliminar</p>
+    <section class="cart-text-price">
+      <select name="" id="">
+        <option value="">1 u.</option>
+      </select>
+      <p>$5000</p>
+    </section>
+  </div>
+</section>
+<section class="card-bot">
+  <p>Envío</p>
+  <p>Gratis</p>
+</section>
+<section class="cart-line">
+  <div class="line"></div>
+</section>
+</article>`;
